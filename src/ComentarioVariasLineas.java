772e3495f5e401f07524ab911ec9fb7c63f40456
12manel123 <manelcastellvicerezuela@gmail.com>
2023-09-29 17:49:57 +0200

    1r comentario

---
+++ src/ComentarioVariasLineas.java
@@ -0,0 +1,11 @@
+
+public class ComentarioVariasLineas {
+	public static void main(String[] args) {
+		/*System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DEL PROGRAMA");*/
+		for(int i=0;i<6;/* ahora viene el paso*/ i++)
+			System.out.println(i+" ");
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
